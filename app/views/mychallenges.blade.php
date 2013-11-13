6c2fff449fcc9d640256c30da0ef4fa9cc965e1b
Douglas <douglas@platacore.com.br>
2013-11-12 20:39:22 -0800

    Right amount

--- app/views/mychallenges.blade.php
+++ app/views/mychallenges.blade.php
@@ -29,9 +29,11 @@
 
     $world_total = $world_sum_sql[0]->total;
 
-    $user_sum_sql = DB::select('select sum(t.penalty_per_misses) as total
-                               from teams t, user_fails_challenges fc
+    $user_sum_sql = DB::select('select count(t.penalty_per_misses) as total
+                               from teams t, user_fails_challenges fc, users u
                                where t.challenge_id = fc.challenge_id
+                               AND u.team_id = t.id
+                               AND u.id = fc.user_id
                                AND fc.challenge_id = ?
                                AND fc.user_id = ?'
         , array($challenge->id, Auth::user()->id));
