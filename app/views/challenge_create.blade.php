8961801da56c37ce081aa21861bc3ea3ca28477b
kar2905 <kar2905@gmail.com>
2013-11-12 21:28:01 -0800

    textual changes

--- app/views/challenge_create.blade.php
+++ app/views/challenge_create.blade.php
@@ -6,9 +6,9 @@
 
 <div class="clearfix">
 @foreach ($challenges as $challenge)
-    <div class="challenge">
-        <a href="/user/challenge-add/{{ $challenge->id }}"><img src="{{ $challenge->image_url }}" width="200"></a>
-        <p>{{ $challenge->name }}</p>
+    <div class="challenge" style="width:200px;height:200px">
+        <a href="/user/challenge-add/{{ $challenge->id }}"><img class="challenges" src="{{ $challenge->image_url }}" style="display:block;width:130px;margin-left: auto;margin-right: auto;margin-top:10px;margin-bottom:auto;"></a>
+        <br/><p style="text-align: center;clear:both">{{ $challenge->name }}</p>
     </div>
 @endforeach
 </div>
